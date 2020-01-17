92ed8331bbce642a73f541468c47aeae42181807
motikan2010 <motikan2010@yahoo.co.jp>
2020-01-17 11:34:28 +0900

    first commit

---
+++ src/main/java/com/example/cve_2020_5398/Cve20205398Application.java
@@ -0,0 +1,13 @@
+package com.example.cve_2020_5398;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cve20205398Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cve20205398Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cve_2020_5398/Cve20205398ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cve_2020_5398;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cve20205398ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
